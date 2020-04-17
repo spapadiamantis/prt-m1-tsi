85d300d014b95d5040f91f3697f2bb63dde0e738
Sotiris Papadiamantis <sotirispapadiamantis@gmail.com>
2020-04-17 21:21:03 +0300

    Add main function

--- script.py
+++ script.py
@@ -1,3 +1,12 @@
 """This is a script that contains the filter"""
-
+import sys
+from scipy.io import wavfile
+from scipy import signal
 import numpy as np
+
+
+if __name__ == "__main__":
+    
+    # Read wave file
+    samplerate,signal =  wavfile.read(sys.argv[0], mmap=False)
+    
